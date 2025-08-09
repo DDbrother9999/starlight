02260907a4d4a77df166fb436c46c92b90d0e534
DDbrother <66873499+DDbrother9999@users.noreply.github.com>
2025-08-08 20:26:40 -0400

    feat: moving platforms

---
+++ animatable_body_2d.gd
@@ -0,0 +1,12 @@
+extends AnimatableBody2D
+
+@export var travel: Vector2 = Vector2(300, 0)
+
+@export var duration: float = 4.0
+
+func _ready():
+	var tween = create_tween()
+	
+	tween.set_loops()
+	tween.tween_property(self, "position", position + travel, duration / 2.0)
+	tween.tween_property(self, "position", position, duration / 2.0)
